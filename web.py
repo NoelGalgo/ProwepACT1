20e9cef1ef47b50ae83925b9f1d5bd7a46fdf968
SernaNoel <147465012+NoelGalgo@users.noreply.github.com>
2026-02-11 22:36:15 +0000

    Datos request y response puerto 8000

--- web.py
+++ web.py
@@ -1,13 +1,24 @@
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
+
     def url(self):
         return urlparse(self.path)
 
     def query_data(self):
         return dict(parse_qsl(self.url().query))
+
+    def get_response(self):
+        return f"""
+        <h1> Hola Web </h1>
+        <p> URL Parse Result: {self.url()} </p>
+        <p> Path Original: {self.path} </p>
+        <p> Headers: {self.headers} </p>
+        <p> Query: {self.query_data()} </p>
+        """
 
     def do_GET(self):
         self.send_response(200)
@@ -15,17 +26,8 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(self.get_response().encode("utf-8"))
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
-
 
 if __name__ == "__main__":
-    print("Starting server")
-    server = HTTPServer(("localhost", 8080), WebRequestHandler)
+    print("Servidor escuchando en http://localhost:8000")
+    server = HTTPServer(("localhost", 8000), WebRequestHandler)
     server.serve_forever()
